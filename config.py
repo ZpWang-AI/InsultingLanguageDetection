f4b12fa914cb072f6ffbafd6791e4165a95a278b
wzp <zhipangwang@gmail.com>
2023-03-21 11:41:13 +0000

    update config

--- config.py
+++ config.py
@@ -1,20 +1,42 @@
-class Config:
+class CustomConfig:
     version = 'base'
     
     model_name = 'xlm-roberta-base'
     device = 'cpu'
     cuda_id = '0'
     
-    saved_model_fold = 'saved_model'
-    saved_result_file = 'saved_res.txt'
+    train_data_file = ''
+    dev_data_file = ''
+    test_data_file = ''
+    pretrained_model_fold = ''
+    save_res_fold = ''
+    
+    test_model_path = ''
+    
+    input_feature = 'a'
     
     base = True
+    clip = False
     just_test = False
-    
+        
     epochs = 10
     batch_size = 8
+    save_model_epoch = 1
+    pb_frequency = 10
+    train_ratio = 0.8
+    lr = 5e-5
     
+    def as_list(self):
+        return [[attr, getattr(self, attr)] for attr in dir(self)
+                if not callable(getattr(self, attr)) and not attr.startswith("__")]
     
-def get_default_config():
-    return Config()
+    def as_dict(self):
+        return dict(self.as_list())
+   
+    
+if __name__ == '__main__':
+    sample_config = CustomConfig()
+    print(sample_config.as_list())
+    print(sample_config.as_dict())
+    pass
 
